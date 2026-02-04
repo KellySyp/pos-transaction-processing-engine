10c407ef8150347984b7fde08e1da3d8ffe89361
Kelly <kelmsyp@gmail.com>
2026-02-04 10:55:50 -0500

    Initial commit

---
+++ src/main/java/com/kellysyp/pos_transaction_processing_engine/PosTransactionProcessingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.kellysyp.pos_transaction_processing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTransactionProcessingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTransactionProcessingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kellysyp/pos_transaction_processing_engine/PosTransactionProcessingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellysyp.pos_transaction_processing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTransactionProcessingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
